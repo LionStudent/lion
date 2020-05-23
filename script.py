9042b6d44856cc9b77ab24b3687751e89d410b22
lionstudent <lion.student@protonmail.com>
2020-05-23 21:39:51 +0000

    changed parabola to sine function

--- script.py
+++ script.py
@@ -1,37 +1,47 @@
 import matplotlib
 
 import matplotlib.pyplot as plt
+import math
+
 
 
 matplotlib.use('Agg')
 
-xAxis = list(range(-20,20))
+xAxis = list(range(0,64))
+
+xTenth = []
+
+for x in xAxis:
+
+  xTenth.append(x*0.1)
 
 
-def parabola(xValues):
+xAxis = xTenth
+
+def sine(xValues):
 
   yValues = []
 
   for x in xValues:
 
-    y = x*x
+    y = math.sin(x)
 
     yValues.append(y)
 
   return yValues
 
 
-yAxis = parabola(xAxis)
+yAxis = sine(xAxis)
 
-sliceXAxis  = xAxis[5:35]
+sliceXAxis  = xAxis[:]
 
-sliceYAxis  = yAxis[5:35]
+sliceYAxis  = yAxis[:]
 
-style = 'ro'
+style = '-b'
 
 plt.plot(sliceXAxis , sliceYAxis , style)
 
-plt.axis([-10, 10, 0, 100])
+plt.axis([0,6.4, -1, 1])
 
 filename = 'graph.png'
 
